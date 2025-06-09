3e64ed83278d033f086c8be73fdae1b8bba05f4b
Ivan <ivanr553@gmail.com>
2025-06-08 23:30:20 -0500

    Updating to support latest form updates

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from typing import Optional
+from typing import List, Optional
 from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -12,20 +12,22 @@ CORS(app, resources={r"/rsvp": {"origins": ["https://www.jennaivan.wedding"]}})
 class RSVPData:
     name: str
     email: str
-    children: str
+    number_of_guests: int
     attending: str
-    partner: Optional[str] = None
-    
+    meals: List[str]
+
     def __str__(self):
-        return f"Name:{self.name},partner:{self.partner},Email:{self.email},Children:{self.children},Attending:{self.attending}"
+        return f"Name:{self.name},Email:{self.email},Number Of Guests:{self.number_of_guests},Attending:{self.attending},Meals:{self.meals}"
 
 def validate_rsvp_data(rsvp_data):
-    if not rsvp_data.name or not rsvp_data.email or rsvp_data.attending is None:
+    if not rsvp_data.name or rsvp_data.attending is None:
         return False
     return True
 
-@app.route("/rsvp", methods=["POST"])
+@app.route("/rsvp", methods=["POST", "OPTIONS"])
 def add_rsvp():
+    if request.method == "OPTIONS":
+        return "", 200
     try:
         data = request.json
         rsvp_data = RSVPData(**data)
@@ -35,9 +37,9 @@ def add_rsvp():
         print(e)
         return "Invalid data", 400
     try:
-        stringified_data = str(rsvp_data)
         with open("rsvp.txt", "a") as f:
-            f.write(stringified_data + "\n")
+            json.dump(data, f)
+            f.write("\n")
     except Exception as e:
         print(e)
         return "Error writing to file", 500
